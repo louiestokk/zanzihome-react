3e916f001b439db1c4c84a19296a85119ee680f3
louie.stokk <louie.stokk@sqli.com>
2023-02-21 22:16:50 +0100

    css

--- src/components/Objects.js
+++ src/components/Objects.js
@@ -37,55 +37,53 @@ const Objects = () => {
 
           return (
             <div className="objects" key={id}>
-              <Link to={`/propertys/zanzibar/${id}`} className="objects-link">
-                <img
-                  src={url.includes("firebase") ? url[1] : url[0]}
-                  alt={location}
-                  loading={"lazy"}
-                />
-                <div className="objects-footer-first">
-                  <div className="objects-logo">
-                    <h3 className="object-location-text">{location}</h3>
-                    <div className="logo">
-                      <div className="logo-circle circlar">
-                        <ImHome className="logo-icon ccc" />
-                      </div>
-                      <div
-                        className="logo-text objectsreal-logo"
-                        style={{ marginRight: "0.75rem" }}
-                      >
-                        <h4>ZanziHom</h4>
-                        <p className="pp">e</p>
-                      </div>
+              <img
+                src={url.includes("firebase") ? url[1] : url[0]}
+                alt={location}
+                loading={"lazy"}
+              />
+              <div className="objects-footer-first">
+                <div className="objects-logo">
+                  <h3 className="object-location-text">{location}</h3>
+                  <div className="logo">
+                    <div className="logo-circle circlar">
+                      <ImHome className="logo-icon ccc" />
+                    </div>
+                    <div
+                      className="logo-text objectsreal-logo"
+                      style={{ marginRight: "0.75rem" }}
+                    >
+                      <h4>ZanziHom</h4>
+                      <p className="pp">e</p>
                     </div>
                   </div>
-                  <p>
-                    {type === "House" || type === "Apartment" ? (
-                      <ImHome style={{ color: "#22c55e" }} />
-                    ) : (
-                      <BsSquare
-                        style={{ color: "#22c55e", background: "#22c55e" }}
-                      />
-                    )}{" "}
-                    {desc}
-                  </p>
-                  <div className="objects-footer">
-                    <span>
-                      {to === "Rent" ? `$${price}/week` : `$${price}.000`}
-                    </span>
-                    <span>{size}sqm</span>
-                    <span>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
-                  </div>
                 </div>
-                <div className="objects-btn-container">
-                  <Link to={`/propertys/zanzibar/${id}`}>
-                    <button type="button">Contact</button>
-                  </Link>
-                  <Link to={`/propertys/zanzibar/${id}`}>
-                    <button type="button">Info</button>
-                  </Link>
+                <p>
+                  {type === "House" || type === "Apartment" ? (
+                    <ImHome style={{ color: "#22c55e" }} />
+                  ) : (
+                    <BsSquare
+                      style={{ color: "#22c55e", background: "#22c55e" }}
+                    />
+                  )}{" "}
+                  {desc}
+                </p>
+                <div className="objects-footer">
+                  <span>
+                    {to === "Rent" ? `$${price}/week` : `$${price}.000`}
+                  </span>
+                  <span>{size}sqm</span>
+                  <span>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
                 </div>
-              </Link>
+              </div>
+              <div className="objects-btn-container">
+                <Link to={`/propertys/zanzibar/${id}`}>
+                  <button type="button">Contact</button>
+                </Link>
+                <Link to={`/propertys/zanzibar/${id}`}>
+                  <button type="button">Info</button>
+                </Link>
+              </div>
             </div>
           );
         })}
@@ -117,58 +115,53 @@ const Objects = () => {
             } = object;
             return (
               <div key={index} className="objects">
-                <Link
-                  to={`/propertys/property/${adId}`}
-                  className="objects-link"
-                >
-                  <img src={uri && uri} alt={Adress} loading={"lazy"} />
-                  <div className="objects-footer-first">
-                    <div className="objects-logo">
-                      <h3 className="object-location-text">{Area}</h3>
-                      <div className="logo">
-                        <div className="logo-circle circlar">
-                          <ImHome className="logo-icon ccc" />
-                        </div>
-                        <div
-                          className="logo-text objectsreal-logo"
-                          style={{ marginRight: "0.75rem" }}
-                        >
-                          <h4>ZanziHom</h4>
-                          <p className="pp">e</p>
-                        </div>
+                <img src={uri && uri} alt={Adress} loading={"lazy"} />
+                <div className="objects-footer-first">
+                  <div className="objects-logo">
+                    <h3 className="object-location-text">{Area}</h3>
+                    <div className="logo">
+                      <div className="logo-circle circlar">
+                        <ImHome className="logo-icon ccc" />
+                      </div>
+                      <div
+                        className="logo-text objectsreal-logo"
+                        style={{ marginRight: "0.75rem" }}
+                      >
+                        <h4>ZanziHom</h4>
+                        <p className="pp">e</p>
                       </div>
                     </div>
-                    <p>
-                      {category === "House" || category === "Apartment" ? (
-                        <ImHome style={{ color: "#22c55e" }} />
-                      ) : (
-                        <BsSquare
-                          style={{ color: "#22c55e", background: "#22c55e" }}
-                        />
-                      )}{" "}
-                      {Title}
-                    </p>
-                    <div className="objects-footer">
-                      <span>
-                        {Rent === null && Sell === null
-                          ? `$${Price}.00`
-                          : `$${Price}/week`}
-                      </span>
-                      <span>{Size}sqm</span>
-                      <span>
-                        {category.charAt(0).toUpperCase() + category.slice(1)}
-                      </span>
-                    </div>
                   </div>
-                  <div className="objects-btn-container">
-                    <Link to={`/propertys/property/${adId}`}>
-                      <button type="button">Contact</button>
-                    </Link>
-                    <Link to={`/propertys/property/${adId}`}>
-                      <button type="button">Info</button>
-                    </Link>
+                  <p>
+                    {category === "House" || category === "Apartment" ? (
+                      <ImHome style={{ color: "#22c55e" }} />
+                    ) : (
+                      <BsSquare
+                        style={{ color: "#22c55e", background: "#22c55e" }}
+                      />
+                    )}{" "}
+                    {Title}
+                  </p>
+                  <div className="objects-footer">
+                    <span>
+                      {Rent === null && Sell === null
+                        ? `$${Price}.00`
+                        : `$${Price}/week`}
+                    </span>
+                    <span>{Size}sqm</span>
+                    <span>
+                      {category.charAt(0).toUpperCase() + category.slice(1)}
+                    </span>
                   </div>
-                </Link>
+                </div>
+                <div className="objects-btn-container">
+                  <Link to={`/propertys/property/${adId}`}>
+                    <button type="button">Contact</button>
+                  </Link>
+                  <Link to={`/propertys/property/${adId}`}>
+                    <button type="button">Info</button>
+                  </Link>
+                </div>
               </div>
             );
           })}
