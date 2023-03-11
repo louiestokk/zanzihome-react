8cee48e9a276adf311e4e4e5ed6052fce7b563b2
louie.stokk <louie.stokk@sqli.com>
2023-03-11 17:49:14 +0100

    metacoontent

--- src/pages/Advertise.js
+++ src/pages/Advertise.js
@@ -22,7 +22,7 @@ function Advertise() {
         <meta
           name="description"
           content={
-            "Find houses, plots and apartments for sale or for rent. Zanzihome is a site that will help you find your next or first home. Find lots of apartments and houses for rent. Do you have a home, plot or business that you want to rent out or sell? Advertise with us. Find properties in Zanzibar. You can find all types of properties here. Eazy whay to sell or rent your property in Zanzibar. Sell property Zanzibar. Properties for sale in Zanzibar. Business for sale Zanzibar. Business for rent Zanzibar."
+            "Find properties in Zanzibar as houses, plots and apartments for sale or for rent. Zanzihome is a site that will help you find your next or first home. Find lots of apartments and houses for rent. Do you have a home, plot or business that you want to rent out or sell? Advertise with us. Find properties in Zanzibar. You can find all types of properties here. Eazy whay to sell or rent your property in Zanzibar. Sell property Zanzibar. Properties for sale in Zanzibar. Business for sale Zanzibar. Business for rent Zanzibar. Properties for Sale in Zanzibar"
           }
         />
       </Helmet>
