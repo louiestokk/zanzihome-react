637dc022c5d9e857ae4b6736371475001c39f2aa
louie.stokk <louie.stokk@sqli.com>
2023-04-04 23:00:10 +0200

    data scheme homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { useGlobalContext } from "../context";
 import Propertyzanzibar from "./propertyzanzibar";
-import AdBanner from "../components/AdBanner";
-import image from "../utils/zanzibarisland.jpg";
 import { Helmet } from "react-helmet-async";
 const Home = () => {
-  const { query, handleChange, handleSubmit } = useGlobalContext();
+  const articleStructuredData = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    headline: "Guide: Buy property in Zanzibar",
+    description:
+      "Properties for Sale and Rent in Zanzibar. Discover Your Dream Property for Sale in Zanzibar. Advertise Your Property on Zanzihome Today! Your One-Stop Property Marketplace!",
+    image:
+      "https://images.pexels.com/photos/14667295/pexels-photo-14667295.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    datePublished: new Date("2023-04-04T09:25:01.340Z").toISOString(),
+    author: {
+      "@type": "Person",
+      name: "Louie Stokk"
+    }
+  };
   const [size, setSize] = useState(window.innerWidth);
-
   useEffect(() => {
     const detectSize = window.addEventListener("resize", (e) => {
       setSize(document.body.clientWidth);
@@ -18,6 +27,9 @@ const Home = () => {
   }, [size]);
   return (
     <div>
+      <script type="application/ld+json">
+        {JSON.stringify(articleStructuredData)}
+      </script>
       <Helmet>
         <meta charSet="utf-8" />
         <title>{"Zanzihome Properties in Zanzibar"}</title>
