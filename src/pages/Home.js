b3339f683211fcdc0d486f7a44e4147d3bdfa12c
louie.stokk <louie.stokk@sqli.com>
2023-04-04 23:07:44 +0200

    ee

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,6 @@ import React, { useState, useEffect } from "react";
 import Propertyzanzibar from "./propertyzanzibar";
 import { Helmet } from "react-helmet-async";
 const Home = () => {
-  const articleStructuredData = {
-    "@context": "https://schema.org",
-    "@type": "Article",
-    headline: "Guide: Buy property in Zanzibar",
-    description:
-      "Properties for Sale and Rent in Zanzibar. Discover Your Dream Property for Sale in Zanzibar. Advertise Your Property on Zanzihome Today! Your One-Stop Property Marketplace!",
-    image:
-      "https://images.pexels.com/photos/14667295/pexels-photo-14667295.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    datePublished: new Date("2023-04-04T09:25:01.340Z").toISOString(),
-    author: {
-      "@type": "Person",
-      name: "Louie Stokk"
-    }
-  };
   const [size, setSize] = useState(window.innerWidth);
   useEffect(() => {
     const detectSize = window.addEventListener("resize", (e) => {
@@ -27,9 +13,6 @@ const Home = () => {
   }, [size]);
   return (
     <div>
-      <script type="application/ld+json">
-        {JSON.stringify(articleStructuredData)}
-      </script>
       <Helmet>
         <meta charSet="utf-8" />
         <title>{"Zanzihome Properties in Zanzibar"}</title>
--- src/pages/propertyzanzibar.js
+++ src/pages/propertyzanzibar.js
@@ -8,8 +8,25 @@ import BannerSection from "../components/BannerSection";
 import Faq from "../components/Faq";
 import Abovefooter from "../components/Abovefooter";
 const propertyzanzibar = () => {
+  const articleStructuredData = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    headline: "Properties for Sale and Rent in Zanzibar",
+    description:
+      "Properties for Sale and Rent in Zanzibar. Discover Your Dream Property for Sale in Zanzibar. Advertise Your Property on Zanzihome Today! Your One-Stop Property Marketplace!",
+    image:
+      "https://images.pexels.com/photos/14667295/pexels-photo-14667295.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    datePublished: new Date("2023-04-04T09:25:01.340Z").toISOString(),
+    author: {
+      "@type": "Person",
+      name: "Louie Stokk"
+    }
+  };
   return (
     <>
+      <script type="application/ld+json">
+        {JSON.stringify(articleStructuredData)}
+      </script>
       <Helmet>
         <meta charSet="utf-8" />
         <title>
