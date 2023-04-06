aed78e98539f90a688cdc18711d3b7abbe401417
louie.stokk <louie.stokk@sqli.com>
2023-04-06 20:23:13 +0200

    fix bug

--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import objectReducer from "./objects/objectSlice";
 import imagesRedducer from "./ImagesSlice";
 import firebaseReducer from "./firebaseDataSlice";
+
 export const store = configureStore({
   reducer: {
     object: objectReducer,
