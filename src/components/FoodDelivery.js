c89c62b2f4834c6976d877193afb269f38797979
louiestokk <louiestokk@gmail.com>
2025-11-03 13:56:05 +0300

    link

--- src/components/FoodDelivery.js
+++ src/components/FoodDelivery.js
@@ -3,7 +3,7 @@ import React from 'react'
 const FoodDelivery = () => {
   return (
     <div>
-          <a alt='food delivery zanzibar' href={'https://www.forkio.app/'}>
+          <a alt='food delivery zanzibar' href={'https://maps.app.goo.gl/Y8jWpZjka6LXeN1b9'}>
                 <img loading='lazy' style={{width:'100%'}} src='https://i.ibb.co/tMRt45KK/forkio-banner.png' alt='food delivery in Zanzibar'/>
         </a>
     </div>
